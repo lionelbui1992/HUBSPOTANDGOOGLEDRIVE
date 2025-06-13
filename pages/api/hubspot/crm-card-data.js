7afb266f6d8567dece9a6d2ca12db240a1a53b6d
hyperspacehq <andrewt@hyperspacehq.com>
2025-06-14 00:39:50 +0700

    code 13062025

--- pages/api/hubspot/crm-card-data.js
+++ pages/api/hubspot/crm-card-data.js
@@ -1,22 +1,26 @@
 export default function handler(req, res) {
+  const { hs_object_id } = req.body;
 
+  if (!hs_object_id) {
+    return res.status(400).json({ error: "Missing hs_object_id" });
+  }
+
+  const dealId = hs_object_id;
 
   res.status(200).json({
-  "results": [
-    {
-      "objectId": 245,
-      "title": "📁 Google Drive Folder",
-      "link": "https://gdrive.onextdigital.com/createfolder?dealid=12345",
+    results: [
+      {
+        objectId: 245,
+        title: "📁 Google Drive Folder",
+        link: `https://gdrive.onextdigital.com/createfolder?dealid=${dealId}`,
+      }
+    ],
+    primaryAction: {
+      type: "IFRAME",
+      width: 890,
+      height: 748,
+      uri: `https://gdrive.onextdigital.com/createfolder?dealid=${dealId}`,
+      label: "🔐 Google Authen",
     }
-  ],
-  "primaryAction": {
-    "type": "IFRAME",
-    "width": 890,
-    "height": 748,
-    "uri": "https://gdrive.onextdigital.com/createfolder?dealid=12345",
-    "label": "🔐 Google Authen",
-  }
-});
+  });
 }
-
-
