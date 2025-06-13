21328640399c96b1ee473cdd9ec59a5be4f2522c
hyperspacehq <andrewt@hyperspacehq.com>
2025-06-14 00:38:16 +0700

    code 13062025

--- pages/api/hubspot/crm-card-data.js
+++ pages/api/hubspot/crm-card-data.js
@@ -1,26 +1,22 @@
 export default function handler(req, res) {
-  const { associatedObjectId } = req.body; // lấy dealId từ HubSpot CRM
 
-  if (!associatedObjectId) {
-    return res.status(400).json({ error: "Missing associatedObjectId (dealid)" });
-  }
-
-  const dealId = associatedObjectId;
 
   res.status(200).json({
-    results: [
-      {
-        objectId: 245,
-        title: "📁 Google Drive Folder",
-        link: `https://gdrive.onextdigital.com/createfolder?dealid=${dealId}`,
-      }
-    ],
-    primaryAction: {
-      type: "IFRAME",
-      width: 890,
-      height: 748,
-      uri: `https://gdrive.onextdigital.com/createfolder?dealid=${dealId}`,
-      label: "🔐 Google Authen",
+  "results": [
+    {
+      "objectId": 245,
+      "title": "📁 Google Drive Folder",
+      "link": "https://gdrive.onextdigital.com/createfolder?dealid=12345",
     }
-  });
+  ],
+  "primaryAction": {
+    "type": "IFRAME",
+    "width": 890,
+    "height": 748,
+    "uri": "https://gdrive.onextdigital.com/createfolder?dealid=12345",
+    "label": "🔐 Google Authen",
+  }
+});
 }
+
+
