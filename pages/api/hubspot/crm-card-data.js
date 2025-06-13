b3123c0457bc286bf22cc5e337c753a778fb422c
hyperspacehq <andrewt@hyperspacehq.com>
2025-06-14 01:12:35 +0700

    code 13062025

--- pages/api/hubspot/crm-card-data.js
+++ pages/api/hubspot/crm-card-data.js
@@ -1,33 +1,14 @@
 export default function handler(req, res) {
-  const {
-    hs_object_id,
-    firstname,
-    lastname,
-    email,
-    hubspot_owner_id,
-    createdate,
-    hs_lead_status,
-    notes_last_updated
-  } = req.body;
-
-  console.log('🔔 Received from HubSpot:', req.body);
-
-  // Kiểm tra có id không
-  if (!hs_object_id) {
-   // return res.status(400).json({ error: "Missing hs_object_id" });
-  }
-
-  const contactId = hs_object_id;
-  const displayName = `${firstname ?? ''} ${lastname ?? ''}`.trim() || email;
+  
   const driveUrl = `https://gdrive.onextdigital.com/createfolder`;
 
   res.status(200).json({
     results: [
       {
         objectId: contactId,
-        title: `📁 Google Drive Folder cho ${displayName}`,
+        title: `📁 Google Drive Folder}`,
         link: driveUrl,
-        description: `Thư mục Drive được liên kết với ${displayName}`,
+        description: `Thư mục Drive được liên kết`,
         image: "https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_48dp.png"
       }
     ],
