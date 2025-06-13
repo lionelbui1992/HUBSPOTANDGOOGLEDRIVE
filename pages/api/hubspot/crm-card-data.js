4bace6bf670fdd2187d5b2eefe350acd6e9ad50f
hyperspacehq <andrewt@hyperspacehq.com>
2025-06-13 23:37:55 +0700

    code 13062025

--- pages/api/hubspot/crm-card-data.js
+++ pages/api/hubspot/crm-card-data.js
@@ -1,23 +1,14 @@
 export default function handler(req, res) {
-
-
   res.status(200).json({
+    title: "Custom Iframe Card",
     results: [
       {
-        "title": "New CRM Card",
-        "fetch": {
-          "targetUrl": "https://www.example.com/demo-fetch",
-          "objectTypes": [
-            {
-              "name": "contacts",
-              "propertiesToSend": [
-                "firstname",
-                "email",
-                "lastname"
-              ]
-            }
-          ]
-        }
+        type: "IFRAME",
+        width: 890,
+        height: 748,
+        uri: "https://gdrive.onextdigital.com/contact-card",
+        label: "Edit",
+        associatedObjectProperties: ["some_crm_property"]
       }
     ]
   });
